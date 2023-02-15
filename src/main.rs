7702b1318cec805fa40feb81ad5aa2eafc217f29
kalifun <37646342+kalifun@users.noreply.github.com>
2023-02-15 12:24:43 +0800

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+mod cmd;
+use cmd::cli::Cli;
+
 fn main() {
-    println!("Hello! Here is agrm.\n A local git repositories manager.");
+    let matches = Cli::new().cmds.get_matches();
+    match matches.subcommand() {
+        Some(("clone", sub_matches)) => {
+            println!(
+                "Cloning {}",
+                sub_matches.get_one::<String>("REMOTE").expect("required")
+            );
+        }
+        _ => unreachable!(),
+    }
 }
