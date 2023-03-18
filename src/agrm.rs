c00e9222d4ae7d2874c667cc0235c2fd758881dc
acehinnnqru <acehinnnqru@gmail.com>
2023-03-18 19:08:37 +0800

    feat: move init into agrm

--- src/agrm.rs
+++ src/agrm.rs
@@ -4,10 +4,26 @@ use clap::crate_name;
 pub static AGRM_NAME: &str = crate_name!();
 
 pub fn run() -> ! {
+    init();
+
     let cli = Cli::new();
     let settings = load_settings(&cli);
 
     cli.run(&settings)
+}
+
+pub fn init() {
+    init_logger();
+}
+
+fn init_logger() {
+    let env = env_logger::Env::default().filter_or("AGRM_LOG", "info");
+
+    env_logger::Builder::from_env(env)
+        .format_timestamp(None)
+        .format_target(false)
+        .format_module_path(false)
+        .init();
 }
 
 fn load_settings(cli: &Cli) -> Settings {
