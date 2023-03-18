5782f5d5417026440072da741d8ae1559c755949
acehinnnqru <acehinnnqru@gmail.com>
2023-03-18 19:08:37 +0800

    feat: move init into agrm

--- src/main.rs
+++ src/main.rs
@@ -5,18 +5,6 @@ pub mod settings;
 #[macro_use]
 extern crate log;
 
-fn init_logger() {
-    let env = env_logger::Env::default().filter_or("AGRM_LOG", "info");
-
-    env_logger::Builder::from_env(env)
-        .format_timestamp(None)
-        .format_target(false)
-        .format_module_path(false)
-        .init();
-}
-
 fn main() {
-    init_logger();
-
     agrm::run()
 }
