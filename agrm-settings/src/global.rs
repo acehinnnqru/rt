6babda313ea5635bb309ace5c7a5f4cc5245aa70
acehinnnqru <acehinnnqru@gmail.com>
2023-09-07 22:39:46 +0800

    feat: remove redundant content

--- agrm-settings/src/global.rs
+++ agrm-settings/src/global.rs
@@ -1,27 +1,14 @@
 use crate::AGRM_NAME;
 use serde::Deserialize;
 
-#[derive(Deserialize, Debug, PartialEq, Eq, Hash, Clone, Copy)]
-#[serde(rename_all = "snake_case")]
-pub enum StructureNode {
-    OSUser,
-    Platform,
-    NameSpace,
-    Name,
-    Branch,
-    Tag,
-}
-
 #[derive(Deserialize, Debug, PartialEq)]
 pub struct GlobalSettings {
-    pub structure: Option<Vec<StructureNode>>,
     pub root: Option<String>,
 }
 
 impl Clone for GlobalSettings {
     fn clone(&self) -> Self {
         Self {
-            structure: self.structure.clone(),
             root: self.root.clone(),
         }
     }
@@ -47,10 +34,8 @@ fn os_default_root() -> String {
 
 impl Default for GlobalSettings {
     fn default() -> Self {
-        use StructureNode::*;
         Self {
             root: Some(os_default_root()),
-            structure: Some(vec![Platform, NameSpace, Name]),
         }
     }
 }
@@ -58,7 +43,6 @@ impl Default for GlobalSettings {
 impl GlobalSettings {
     pub fn merge(mut self, target: Option<GlobalSettings>) -> GlobalSettings {
         if let Some(target) = target {
-            self.structure = self.structure.or(target.structure);
             self.root = self.root.or(target.root);
         }
         self
