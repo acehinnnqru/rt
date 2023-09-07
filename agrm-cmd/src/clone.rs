e3e96c697aaa71106b27e70955f1f1604102cb1d
acehinnnqru <acehinnnqru@gmail.com>
2023-09-07 22:39:24 +0800

    feat(cmd): remove redundant content

--- agrm-cmd/src/clone.rs
+++ agrm-cmd/src/clone.rs
@@ -1,44 +1,45 @@
 use clap::Args;
+use std::path::Path;
 use std::thread;
 
 use crate::settings::Settings;
 extern crate clap;
 use crate::helper;
-use crate::manager::RepRoot;
 
 #[derive(Args, Debug)]
 pub struct CloneArgs {
-    #[arg(short, long = "p", help = "The platform to clone from")]
-    platform: String,
-
-    #[arg(long = "ns", help = "The namespace to clone from")]
-    namespace: Option<String>,
-
-    #[arg(help = "The repositories to clone", value_parser, num_args = 1..)]
+    #[arg(help = "The links of repositories to clone", value_parser, num_args = 1..)]
     repositories: Vec<String>,
 }
 
 pub fn run(_setting: &Settings, args: CloneArgs) -> ! {
-    let rep_root: RepRoot = _setting.into();
-    let _ = std::fs::create_dir_all(rep_root.get_root());
+    let global_root = _setting.global.clone().unwrap().root.unwrap().clone();
+    let _ = std::fs::create_dir_all(global_root.as_str());
 
-    let repos = helper::parse_repositories(args.namespace, args.repositories);
+    let repos = args
+        .repositories
+        .into_iter()
+        .map(helper::parse_repository)
+        .collect::<Vec<_>>();
 
     let mut handles = vec![];
 
-    repos.into_iter().for_each(|(ns, name)| {
-        let rep_root = rep_root.clone();
-        let platform = args.platform.clone();
-
+    repos.into_iter().for_each(|repo| {
+        let global_root = global_root.clone();
         let handle = thread::spawn(move || {
-            let target_path = rep_root.gen_target_path(&platform, &ns, &name);
-            println!("Cloning {}:{}/{} to {:?}", &platform, ns, name, target_path);
-            if let Err(e) = agrm_core::git::clone_default_branch(
-                format!("https://{}/{}/{}", &platform, ns, name).as_str(),
-                target_path.as_path().to_str().unwrap(),
-            ) {
-                eprintln!("{}", e);
-            };
+            if let Some(repo) = repo {
+                let target_path = Path::new(global_root.clone().as_str())
+                    .join(repo.get_host())
+                    .join(repo.get_namespace())
+                    .join(repo.get_name());
+
+                if let Err(e) = agrm_core::git::clone_default_branch(
+                    repo.get_ssh_url().as_str(),
+                    target_path.as_path().to_str().unwrap(),
+                ) {
+                    eprintln!("{}", e);
+                };
+            }
         });
         handles.push(handle);
     });
--- agrm-cmd/src/helper.rs
+++ agrm-cmd/src/helper.rs
@@ -1,31 +1,94 @@
-const REPONAME_REGEX: &str = r"^[a-zA-Z0-9\-_\.]+$";
-const NS_AND_NAME_REGEX: &str = r"^[a-zA-Z0-9\-_\.]+/[a-zA-Z0-9\-_\.]+$";
+pub struct Repository {
+    host: String,
+    namespace: String,
+    name: String,
+}
 
-// return Vector of (namespace, name)
-pub fn parse_repositories(namespace: Option<String>, repos: Vec<String>) -> Vec<(String, String)> {
-    let mut v = Vec::<(String, String)>::new();
-    let regex_str = match namespace {
-        Some(_) => REPONAME_REGEX.to_string(),
-        None => NS_AND_NAME_REGEX.to_string(),
-    };
+impl Repository {
+    pub fn get_host(&self) -> &str {
+        &self.host
+    }
 
-    let re = regex::Regex::new(&regex_str).unwrap();
+    pub fn get_namespace(&self) -> &str {
+        &self.namespace
+    }
 
-    repos
-        .iter()
-        .filter_map(|r| {
-            if !re.is_match(r) {
-                eprintln!("Invalid repository name: {}", r);
-                return None;
+    pub fn get_name(&self) -> &str {
+        &self.name
+    }
+
+    pub fn get_full_name(&self) -> String {
+        format!("{}/{}", self.get_namespace(), self.get_name())
+    }
+
+    pub fn get_full_name_with_host(&self) -> String {
+        format!("{}/{}", self.get_host(), self.get_full_name())
+    }
+
+    pub fn get_ssh_url(&self) -> String {
+        format!(
+            "git@{}:{}/{}",
+            self.get_host(),
+            self.get_namespace(),
+            self.get_name()
+        )
+    }
+
+    pub fn get_https_url(&self) -> String {
+        format!("https://{}/{}", self.get_host(), self.get_full_name())
+    }
+}
+
+// Parse a repository string into a `Repository`.
+// The format of a repository maybe like this:
+// - ssh format: `git@<host>:<namespace>/<name>` or `git@<host>:<namespace>/<name>.git`
+// - https format: `https://<host>/<namespace>/<name>` or `https://<host>/<namespace>/<name>.git`
+// - simplely `<host>/<namespace>/<name>` or `<host>/<namespace>/<name>.git`
+pub fn parse_repository(repo: String) -> Option<Repository> {
+    match repo.find('@') {
+        Some(_) => {
+            // ssh format
+            parse_ssh_format(&repo)
+        }
+        None => {
+            // https format
+            match repo.find("https://") {
+                Some(_) => parse_https_format(&repo),
+                None => parse_simple_format(&repo),
             }
-            Some(match &namespace {
-                Some(ns) => (ns.to_string(), r.to_string()),
-                None => {
-                    let v: Vec<&str> = r.split('/').collect();
-                    (v[0].to_string(), v[1].to_string())
-                }
-            })
-        })
-        .for_each(|r| v.push(r));
-    v
+        }
+    }
+}
+
+fn parse_ssh_format(repo: &str) -> Option<Repository> {
+    let mut split = repo.split('@');
+    let host = split.next().unwrap();
+    let mut split = split.next().unwrap().split(':');
+    let namespace = split.next().unwrap();
+    let name = split.next().unwrap();
+    Some(Repository {
+        host: host.to_string(),
+        namespace: namespace.to_string(),
+        name: name.to_string(),
+    })
+}
+
+fn parse_simple_format(repo: &str) -> Option<Repository> {
+    let mut split = repo.split('/');
+    let host = split.next().unwrap();
+    let namespace = split.next().unwrap();
+    let mut name = split.next().unwrap();
+    if name.ends_with(".git") {
+        name = &name[..name.len() - 4];
+    }
+    Some(Repository {
+        host: host.to_string(),
+        namespace: namespace.to_string(),
+        name: name.to_string(),
+    })
+}
+
+fn parse_https_format(repo: &str) -> Option<Repository> {
+    let mut split = repo.split("https://");
+    parse_simple_format(split.next().unwrap())
 }
