6940f01efc11a29063f504eeb591cec58ccd724f
acehinnnqru <acehinnnqru@gmail.com>
2024-08-14 23:29:52 +0800

    feat: add config mod

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,22 @@ use std::str::FromStr;
 mod repository;
 use repository::Repository;
 
-fn main() {
-    let repo = std::env::args().nth(1).expect("no repository given");
+mod config;
 
-    println!("repository: {:?}", Repository::from_str(&repo));
+fn main() {
+    let path = std::env::args().nth(1).expect("no repository given");
+
+    let repo = Repository::from_str(&path).unwrap();
+
+    println!("\nparsed repository: {:?}\n", repo);
+
+    config::init();
+
+    let root = config::root();
+
+    if root.trim().is_empty() {
+        unreachable!("invalide root setting")
+    }
+
+    println!("root: {}", root);
 }
