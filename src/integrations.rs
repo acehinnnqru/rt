91845f7360f39ab252a3a9e032f61bdd545a0c71
acehinnnqru <acehinnnqru@gmail.com>
2024-08-15 00:39:55 +0800

    feat: done

--- src/integrations.rs
+++ src/integrations.rs
@@ -1,3 +1,7 @@
-pub fn zoxide(path: &str) {
+use std::path::Path;
 
+use crate::cmd::exec_stream;
+
+pub fn zoxide(path: &Path) {
+    exec_stream(format!("zoxide add {}", path.to_str().unwrap()).as_str())
 }
