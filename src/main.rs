a497588789c875a3a66892e7b9415c560de9a2bd
acehinnnqru <acehinnnqru@gmail.com>
2024-08-14 10:40:05 +0800

    feat: move Repository to mod

--- src/main.rs
+++ src/main.rs
@@ -1,47 +1,10 @@
-use regex::bytes::Regex;
 use std::str::FromStr;
+
+mod repository;
+use repository::Repository;
 
 fn main() {
     let repo = std::env::args().nth(1).expect("no repository given");
 
     println!("repository: {:?}", Repository::from_str(&repo));
-}
-
-#[derive(Debug)]
-struct Repository {
-    platform: String,
-    namespace: String,
-    name: String,
-}
-
-impl Repository {
-    fn new(platform: &[u8], namespace: &[u8], name: &[u8]) -> Self {
-        Self {
-            platform: String::from_utf8(platform.to_vec()).unwrap(),
-            namespace: String::from_utf8(namespace.to_vec()).unwrap(),
-            name: String::from_utf8(name.to_vec()).unwrap(),
-        }
-    }
-}
-
-#[derive(Debug, PartialEq, Eq)]
-struct ParseRepositoryError;
-
-impl FromStr for Repository {
-    type Err = ParseRepositoryError;
-
-    fn from_str(s: &str) -> Result<Self, Self::Err> {
-        let mut pattern = Regex::new(r"git@(.*):(.*)/(.*).git").unwrap();
-        let sbytes = s.as_bytes();
-        if !pattern.is_match(sbytes) {
-            pattern = Regex::new(r"http[s]?://(.*)/(.*)/(.*).git").unwrap();
-            if !pattern.is_match(sbytes) {
-                unreachable!("invalid format of repository")
-            }
-        }
-
-        let (_, [platform, namespace, name]) = pattern.captures(sbytes).unwrap().extract();
-
-        Ok(Repository::new(platform, namespace, name))
-    }
 }
---
+++ src/repository.rs
@@ -0,0 +1,41 @@
+use std::str::FromStr;
+
+use regex::Regex;
+
+#[derive(Debug)]
+pub struct Repository {
+    platform: String,
+    namespace: String,
+    name: String,
+}
+
+impl Repository {
+    fn new(platform: &str, namespace: &str, name: &str) -> Self {
+        Self {
+            platform: String::from_str(platform).unwrap(),
+            namespace: String::from_str(namespace).unwrap(),
+            name: String::from_str(name).unwrap(),
+        }
+    }
+}
+
+#[derive(Debug, PartialEq, Eq)]
+pub struct ParseRepositoryError;
+
+impl FromStr for Repository {
+    type Err = ParseRepositoryError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let mut pattern = Regex::new(r"git@(.*):(.*)/(.*).git").unwrap();
+        if !pattern.is_match(s) {
+            pattern = Regex::new(r"http[s]?://(.*)/(.*)/(.*).git").unwrap();
+            if !pattern.is_match(s) {
+                unreachable!("invalid format of repository")
+            }
+        }
+
+        let (_, [platform, namespace, name]) = pattern.captures(s).unwrap().extract();
+
+        Ok(Repository::new(platform, namespace, name))
+    }
+}
