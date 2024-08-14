0fef85141fbe2515e5e81bc38ed5cba3ce5169d1
acehinnnqru <acehinnnqru@gmail.com>
2024-08-15 00:23:45 +0800

    feat: clone bare done

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
-use std::str::FromStr;
+use std::{path::Path, str::FromStr};
 
 mod repository;
 use repository::Repository;
 
+mod git;
 mod config;
 
 fn main() {
@@ -21,4 +22,19 @@ fn main() {
     }
 
     println!("root: {}", root);
+
+    let target_dir = Path::new(&root)
+        .join(&repo.platform)
+        .join(&repo.namespace)
+        .join(&repo.name);
+
+    let ssh = format!("git@{}:{}/{}", repo.platform, repo.namespace, repo.name);
+
+    git::clone_bare(&ssh, &target_dir.join(".bare"));
+
+    write_dot_git(&target_dir, "gitdir: ./.bare")
+}
+
+fn write_dot_git(dir: &Path, content: &str) {
+    std::fs::write(dir.join(".git"), content).unwrap();
 }
